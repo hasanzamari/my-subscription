55de1bef3689a7c43b113c82e338c0971d78ff0e
Trova.Officer <45694710+hasanzamari@users.noreply.github.com>
2026-06-10 22:57:48 +0330

    github_paths.py

---
+++ Core/github_paths.py
@@ -0,0 +1,33 @@
+COMMON_PATHS = [
+
+    "sub.txt",
+    "subs.txt",
+    "subscription.txt",
+    "subscriptions.txt",
+
+    "all.txt",
+    "mix.txt",
+
+    "config.txt",
+    "configs.txt",
+
+    "proxy.txt",
+    "vpn.txt",
+
+    "urls.json",
+    "url.json",
+
+    "sub.json",
+    "subscription.json",
+
+    "output/all.txt",
+    "output/sub.txt",
+    "output/v2ray.txt",
+
+    "base64.txt",
+    "base64/all.txt",
+
+    "share.txt",
+
+    "README.md"
+]
