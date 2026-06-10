739de65dc477fcbc87bff32bc93de92a0b51a00b
Trova.Officer <45694710+hasanzamari@users.noreply.github.com>
2026-06-10 23:02:05 +0330

    github_paths.py

--- Core/github_paths.py
+++ Core/github_paths.py
@@ -1,33 +1,60 @@
 COMMON_PATHS = [
 
-    "sub.txt",
-    "subs.txt",
-    "subscription.txt",
-    "subscriptions.txt",
+# Root
+"sub.txt",
+"subs.txt",
+"subscription.txt",
+"subscriptions.txt",
+"all.txt",
+"mix.txt",
+"config.txt",
+"configs.txt",
+"proxy.txt",
+"vpn.txt",
+"share.txt",
+"links.txt",
 
-    "all.txt",
-    "mix.txt",
+# JSON
+"urls.json",
+"url.json",
+"sub.json",
+"subscription.json",
+"configs.json",
 
-    "config.txt",
-    "configs.txt",
+# Output
+"output/all.txt",
+"output/sub.txt",
+"output/mix.txt",
+"output/config.txt",
+"output/v2ray.txt",
+"output/vless.txt",
+"output/vmess.txt",
+"output/trojan.txt",
+"output/ss.txt",
+"output/ssr.txt",
+"output/hy2.txt",
+"output/hysteria.txt",
+"output/tuic.txt",
 
-    "proxy.txt",
-    "vpn.txt",
+# subscriptions
+"subscriptions/all_sub.txt",
+"subscriptions/super-sub.txt",
+"subscriptions/sub.txt",
+"subscriptions/v2ray/all_sub.txt",
+"subscriptions/v2ray/super-sub.txt",
+"subscriptions/v2ray/sub.txt",
 
-    "urls.json",
-    "url.json",
+# source
+"source/config/urls.json",
+"source/urls.json",
+"source/sub.txt",
+"source/all.txt",
 
-    "sub.json",
-    "subscription.json",
+# base64
+"base64.txt",
+"BASE64/BLACK_FULL.txt",
+"BASE64/WHITE_FULL.txt",
 
-    "output/all.txt",
-    "output/sub.txt",
-    "output/v2ray.txt",
-
-    "base64.txt",
-    "base64/all.txt",
-
-    "share.txt",
-
-    "README.md"
+# common
+"README.md"
 ]
