2e07c5972f7e230a70bbca12219a63f9ce8322c5
Trova.Officer <45694710+hasanzamari@users.noreply.github.com>
2026-06-10 22:41:23 +0330

    github_queries.py

--- Core/github_queries.py
+++ Core/github_queries.py
@@ -1,52 +1,94 @@
 QUERIES = [
 
-    "v2ray subscription",
+# v2ray
+"v2ray",
+"v2ray config",
+"v2ray configs",
+"v2ray subscription",
+"v2ray subscriptions",
+"v2ray sub",
+"v2ray share",
+"v2ray links",
 
-    "v2ray configs",
+# xray
+"xray",
+"xray config",
+"xray configs",
+"xray subscription",
+"xray subscriptions",
+"xray sub",
 
-    "vless subscription",
+# vmess
+"vmess",
+"vmess config",
+"vmess subscription",
+"vmess share",
 
-    "vmess subscription",
+# vless
+"vless",
+"vless config",
+"vless subscription",
+"vless share",
 
-    "trojan subscription",
+# trojan
+"trojan",
+"trojan config",
+"trojan subscription",
 
-    "clash subscription",
+# shadowsocks
+"shadowsocks",
+"ss subscription",
+"ss config",
+"ssr subscription",
 
-    "mihomo subscription",
+# sing-box
+"sing-box",
+"sing-box subscription",
+"singbox config",
 
-    "sing-box subscription",
+# mihomo
+"mihomo",
+"mihomo subscription",
 
-    "free vpn subscription",
+# clash
+"clash",
+"clash config",
+"clash subscription",
+"clash meta",
 
-    "proxy subscription",
+# hysteria
+"hysteria",
+"hysteria2",
+"hy2",
+"hy2 subscription",
 
-    "xray subscription",
+# tuic
+"tuic",
+"tuic subscription",
 
-    "wireguard subscription",
+# wireguard
+"wireguard",
+"wg config",
+"wireguard subscription",
 
-    "hy2 subscription",
-
-    "hysteria subscription",
-
-    "tuic subscription",
-
-    "base64 subscription",
-
-    "sub.txt",
-
-    "all.txt",
-
-    "mix.txt",
-
-    "subscription.json",
-
-    "urls.json",
-
-    "configs.txt",
-
-    "proxy.txt",
-
-    "vpn.txt",
-
-    "share link"
+# عمومی
+"proxy",
+"proxy list",
+"proxy subscription",
+"free proxy",
+"vpn",
+"vpn config",
+"vpn subscription",
+"free vpn",
+"public vpn",
+"public proxy",
+"share link",
+"node",
+"nodes",
+"airport",
+"base64 subscription",
+"subscription json",
+"urls json",
+"all txt",
+"mix txt"
 ]
