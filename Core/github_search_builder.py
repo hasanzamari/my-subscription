ceba09a8fbebd2f334d7d978a1d9add03a56f08a
Trova.Officer <45694710+hasanzamari@users.noreply.github.com>
2026-06-10 22:42:08 +0330

    github_search_builder.py

--- Core/github_search_builder.py
+++ Core/github_search_builder.py
@@ -3,16 +3,18 @@ from Core.github_queries import QUERIES
 
 def build():
 
-    urls = []
+    searches = []
 
     for q in QUERIES:
 
         q = q.replace(" ", "+")
 
-        urls.append(
-
-            f"https://github.com/search?q={q}&type=code"
-
+        searches.append(
+            f"https://github.com/search?q={q}&type=repositories"
         )
 
-    return urls
+        searches.append(
+            f"https://github.com/search?q={q}&type=code"
+        )
+
+    return list(dict.fromkeys(searches))
