baa3e39992ce17984d2bc6ca8bbea854e4674a9a
Trova.Officer <45694710+hasanzamari@users.noreply.github.com>
2026-06-10 22:36:47 +0330

    github_queries.py

---
+++ Core/github_queries.py
@@ -0,0 +1,52 @@
+QUERIES = [
+
+    "v2ray subscription",
+
+    "v2ray configs",
+
+    "vless subscription",
+
+    "vmess subscription",
+
+    "trojan subscription",
+
+    "clash subscription",
+
+    "mihomo subscription",
+
+    "sing-box subscription",
+
+    "free vpn subscription",
+
+    "proxy subscription",
+
+    "xray subscription",
+
+    "wireguard subscription",
+
+    "hy2 subscription",
+
+    "hysteria subscription",
+
+    "tuic subscription",
+
+    "base64 subscription",
+
+    "sub.txt",
+
+    "all.txt",
+
+    "mix.txt",
+
+    "subscription.json",
+
+    "urls.json",
+
+    "configs.txt",
+
+    "proxy.txt",
+
+    "vpn.txt",
+
+    "share link"
+]
