65e04a9831ff3b6440e82cc7805a005e7e49bc8f
Trova.Officer <45694710+hasanzamari@users.noreply.github.com>
2026-06-10 09:24:11 +0330

    exporter.py

--- Core/exporter.py
+++ Core/exporter.py
@@ -1,25 +1,50 @@
 import os
 from Core.logger import log
 
-
 OUTPUT_DIR = "output"
-OUTPUT_FILE = os.path.join(OUTPUT_DIR, "all.txt")
+
+FILES = {
+    "vmess://": "vmess.txt",
+    "vless://": "vless.txt",
+    "trojan://": "trojan.txt",
+    "ss://": "ss.txt",
+    "ssr://": "ssr.txt",
+    "hy2://": "hy2.txt",
+    "hysteria://": "hysteria.txt",
+    "tuic://": "tuic.txt",
+    "wg://": "wg.txt",
+    "wireguard://": "wireguard.txt"
+}
 
 
 def export_all(configs):
 
     os.makedirs(OUTPUT_DIR, exist_ok=True)
 
+    # فایل اصلی
     with open(
-        OUTPUT_FILE,
+        os.path.join(OUTPUT_DIR, "all.txt"),
         "w",
         encoding="utf-8"
     ) as f:
 
-        for item in configs:
-            f.write(item.strip())
-            f.write("\n")
+        for c in configs:
+            f.write(c.strip() + "\n")
+
+    # فایل‌های جداگانه
+    for proto, filename in FILES.items():
+
+        with open(
+            os.path.join(OUTPUT_DIR, filename),
+            "w",
+            encoding="utf-8"
+        ) as f:
+
+            for c in configs:
+
+                if c.startswith(proto):
+                    f.write(c.strip() + "\n")
 
     log(
-        f"[EXPORT] {len(configs)} configs -> {OUTPUT_FILE}"
+        f"[EXPORT] total={len(configs)}"
     )
