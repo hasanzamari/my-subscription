e200c5b28c765717f80a9ecbde09ea14a84d1666
Trova.Officer <45694710+hasanzamari@users.noreply.github.com>
2026-06-11 15:01:05 +0330

    performance_stats.py

---
+++ Core/performance_stats.py
@@ -0,0 +1,15 @@
+import time
+
+
+class Timer:
+
+    def __init__(self):
+
+        self.start = time.time()
+
+    def stop(self):
+
+        return round(
+            time.time() - self.start,
+            2
+        )
